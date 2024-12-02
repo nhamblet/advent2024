bbfdfed7278757b20b0cf0f7a1031895185390bb
Nick Hamblet <nick.hamblet@gmail.com>
2024-12-02 14:53:28 -0800

    Day 2 text fixes

--- src/test/scala/day02/Day02Part1Tests.scala
+++ src/test/scala/day02/Day02Part1Tests.scala
@@ -4,12 +4,16 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class Day02Part1Tests extends AnyFunSuite {
 
+    test("Diff works") {
+        assert(Day02Part1.diffs(List(1, 3, 2, 4, 5)) == List(-2, 1, -2, -1))
+    }
+
     test("Monotonic works") {
         assert(!Day02Part1.monotonic(Day02Part1.diffs(List(1, 3, 2, 4, 5))))
     }
 
     test("AllSmall works") {
-        assert(!Day02Part1.allSmall(Day02Part1.diffs(List(1, 3, 2, 4, 5))))
+        assert(Day02Part1.allSmall(Day02Part1.diffs(List(1, 3, 2, 4, 5))))
     }
   
 }
