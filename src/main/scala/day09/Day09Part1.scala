895f17bf51bf5fb139058e53bd4cdc924fb2ca21
Nick Hamblet <nick.hamblet@gmail.com>
2024-12-09 20:23:48 -0800

    Day 9, Part 1

--- src/main/scala/day09/Day09Part1.scala
+++ src/main/scala/day09/Day09Part1.scala
@@ -2,8 +2,47 @@ package day09
 
 object Day09Part1 extends App {
   
-    def solve(in: Day09Input): Int = ???
+    def solve(in: Day09Input): Long = {
+        var fileIdx = 0
+        var gapIdx = 0
+        var inFileIdx = 0
+        var blockIdx = 0l
+        var movedFileIdx = in.files.size - 1
+        var movedInFileIdx = in.files(movedFileIdx)
+        var ret = 0l
+        while (fileIdx < movedFileIdx || (fileIdx == movedFileIdx && inFileIdx < movedInFileIdx)) {
+            // read a file
+            while (inFileIdx < in.files(fileIdx) && (fileIdx < movedFileIdx || inFileIdx < movedInFileIdx)) {
+                ret = ret + (fileIdx * blockIdx)
+                blockIdx = blockIdx + 1
+                inFileIdx = inFileIdx + 1
+            }
+            inFileIdx = 0
+            fileIdx = fileIdx + 1
+            // fill a gap
+            if (gapIdx < in.gaps.size) {
+                var inGapIdx = 0
+                while (inGapIdx < in.gaps(gapIdx)) {
+                    while(inGapIdx < in.gaps(gapIdx) && movedInFileIdx >= 0 && (movedFileIdx > fileIdx || (movedFileIdx == fileIdx && movedInFileIdx > 0))) {
+                        ret = ret + (movedFileIdx * blockIdx)
+                        blockIdx = blockIdx + 1
+                        movedInFileIdx = movedInFileIdx - 1
+                        if (movedInFileIdx <= 0) {
+                            movedFileIdx = movedFileIdx - 1
+                            movedInFileIdx = in.files(movedFileIdx)
+                        }
+                        inGapIdx = inGapIdx + 1
+                    }
+                    if (!(movedFileIdx > fileIdx || (movedFileIdx == fileIdx && movedInFileIdx > 0))) {
+                        inGapIdx = in.gaps(gapIdx) // break
+                    }
+                }
+                gapIdx = gapIdx + 1
+            }
+        }
+        ret
+    }
 
     println("Test: " + solve(Day09Input.test()))
-    // println("Actual: " + solve(Day09Input.actual()))
+    println("Actual: " + solve(Day09Input.actual()))
 }
--- src/main/scala/day09/package.scala
+++ src/main/scala/day09/package.scala
@@ -3,11 +3,14 @@ import scala.io.Source
 
 package object day09 {
 
-  case class Day09Input()
+  case class Day09Input(files: Array[Int], gaps: Array[Int])
 
   object Day09Input {
     def apply(src: BufferedSource): Day09Input = {
-        ???
+        val mingled = src.getLines.next.map(_.toString.toInt) // without .toString it returns ascii values
+        val files = Stream.from(0).map(_ * 2).takeWhile(_ < mingled.size).map(mingled).toArray
+        val gaps = Stream.from(0).map(_ * 2 + 1).takeWhile(_ < mingled.size).map(mingled).toArray
+        Day09Input(files, gaps)
     }
 
     def test(): Day09Input =
